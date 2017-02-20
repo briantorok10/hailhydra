79f0f6669eeda1ea3a1b37fb2a585899ae230df2
Jub7 <jchambers2@ggc.edu>
2017-02-20 17:00:15 -0500

    Made a greeting and joke

--- src/DummyClass.java
+++ src/DummyClass.java
@@ -5,7 +5,9 @@ public class DummyClass
 	public static void main(String[] args)
 	{
 		// TODO Auto-generated method stub
-
+		//how do you find will smith in the snow
+		
+		//you look for fresh prince (prints)
 	}
 
 }
