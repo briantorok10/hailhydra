aab13606df1c6c9ae7d3a0b430a8a52b7166017c
Jub7 <jchambers2@ggc.edu>
2017-02-20 17:15:09 -0500

    made a greeting

--- src/DummyClass.java
+++ src/DummyClass.java
@@ -5,6 +5,7 @@ public class DummyClass
 	public static void main(String[] args)
 	{
 		// TODO Auto-generated method stub
+		System.out.println("Hello, World!");
 		//how do you find will smith in the snow
 		
 		//you look for fresh prince (prints)
