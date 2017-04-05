eb64a19fda7e348e33ed00c3ac454e8d1166d2e6
Jub7 <jchambers2@ggc.edu>
2017-04-05 15:42:08 -0400

    Added room class

---
+++ src/Room.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+
+
+public class Room {
+
+	private int roomID;
+	private String roomName;
+	private String roomDescription;
+	private boolean hasBeenEntered;
+	//private ArrayList<Item> itemList;
+	private ArrayList<Monsters> possibleMonsters;
+	//private Puzzle roomPuzzle;
+	private ArrayList<Room> roomExits;
+	
+	public Room(int roomID, String roomName, String roomDescription, ArrayList<Monsters> possibleMonsters,
+			ArrayList<Room> roomExits) {
+		super();
+		this.roomID = roomID;
+		this.roomName = roomName;
+		this.roomDescription = roomDescription;
+		this.hasBeenEntered = false; //should be false upon game start
+		this.possibleMonsters = possibleMonsters;
+		this.roomExits = roomExits;
+	}
+	
+	public ArrayList<Room> selectRoomOptions() {
+		return this.roomExits;
+	}
+	
+	public String selectRoom() {
+		return this.selectRoomOptions().toString(); //Must be changed for clear user format
+	}
+	
+	public void enterRoom() { //more to be added
+		this.hasBeenEntered = true;
+	}
+	
+	public void exitRoom() {
+		//see system document for details
+	}
+	
+}
